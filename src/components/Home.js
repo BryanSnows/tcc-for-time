ff34a54df08c0fa4990d24f0ea71691c5be27656
bryan neves pinto <bryan.pinto@conecthus.org.br>
2022-10-19 08:52:02 -0400

    feat: adding home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Home = () => (
     <Fragment>
-        <Helmet><title>Home - Quiz App</title></Helmet>
+        <Helmet><title>Home - Petrobowl Games</title></Helmet>
         <div id="home">
             <section>
                 <div style={{ textAlign: 'center' }}>
                     <span className="mdi mdi-cube-outline cube"></span>
                 </div>
-                <h1>Quiz App</h1>
+                <h1>Petrobowl Games</h1>
                 <div className="play-button-container">
                     <ul>
                         <li><Link className="play-button" to="/play/instructions">Play</Link></li>
